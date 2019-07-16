de5d6750a7e6bd28c76502537318338c3658f22c
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 15:09:43 -0500

    Add nav styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Services</li>
-      <li>Testimonials</li>
-      <li>Portfolio</li>
-      <li>Contacts</li>
+  <nav className="hidden md:block bg-black-transparent">
+    <ul className="flex px-12 justify-between lg:justify-start">
+      <li className="text-gray-900 bg-blue-transparent px-4 py-4 uppercase">Home</li>
+      <li className="text-gray-200 px-4 py-4 uppercase">About</li>
+      <li className="text-gray-200 px-4 py-4 uppercase">Services</li>
+      <li className="text-gray-200 px-4 py-4 uppercase">Testimonials</li>
+      <li className="text-gray-200 px-4 py-4 uppercase">Portfolio</li>
+      <li className="text-gray-200 px-4 py-4 uppercase">Contacts</li>
     </ul>
   </nav>
 )
