2328611b4e0ec47459253f9a61795a8031cdf27c
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 15:09:11 -0500

    Add transparent blue and black

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const { colors: { black, blue } } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        black: {
+          ...black,
+          'transparent': 'hsl(0,0%,0%,0.3)',
+        },
+        blue: {
+          ...blue,
+          'transparent': 'hsl(200,100%,50%,0.2)',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
