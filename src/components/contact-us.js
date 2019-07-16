892b65e36aad450b00324398a135ddde9926e2df
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-16 08:12:06 -0500

    Create contact us section with form

---
+++ src/components/contact-us.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import ContactForm from "./contact-form"
+
+const ContactUs = () => (
+  <section>
+    <h2>Contact Us</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Ad animi, architecto vero aliquid maiores voluptatibus.</p>
+    <ContactForm />
+  </section>
+)
+
+export default ContactUs
