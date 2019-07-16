0c9531300995781cb5e0500a34665d217bddc5ef
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 15:19:48 -0500

    Move nav from page to hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,11 @@
 import React from "react"
 
+import Nav from "./nav"
+
 const Hero = () => (
   <section>
     <div>
+      <Nav />
       <h1>Welcome To</h1>
       <h2>YourCompanyName</h2>
     </div>
