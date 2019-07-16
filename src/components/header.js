16412317db9435a48b5231dfba8f093df61db519
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 14:10:12 -0500

    Add header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const Header = () => (
-  <header>
-    <a href="/">YourCompanyName</a>
+  <header className="bg-white h-16 flex items-center md:h-20">
+    <a href="/" className="text-orange-500 uppercase pl-4 md:pl-12">YourCompanyName</a>
   </header>
 )
 
