0644c6a394014c3b87a24058da9fd430a03150db
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 16:55:08 -0500

    Fix subheader font-size

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,7 +7,7 @@ const Hero = () => (
     <Nav />
     <div className="self-center text-center font-serif uppercase md:text-left md:absolute md:right-0 md:bottom-0 md:mr-24 md:mb-16">
       <h1 className="text-5xl text-white">Welcome To</h1>
-      <h2 className="text-3xl text-orange-500">YourCompanyName</h2>
+      <h2 className="text-2xl sm:text-3xl text-orange-500">YourCompanyName</h2>
     </div>
   </section>
 )
