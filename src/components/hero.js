d36b9314582985ced6acc0eaff4c7a27dca04978
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 16:11:14 -0500

    Update hero component styling

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,11 +3,11 @@ import React from "react"
 import Nav from "./nav"
 
 const Hero = () => (
-  <section>
-    <div>
-      <Nav />
-      <h1>Welcome To</h1>
-      <h2>YourCompanyName</h2>
+  <section className="h-144 relative bg-gray-500 flex justify-center">
+    <Nav />
+    <div className="self-center text-center font-serif uppercase md:text-left md:absolute md:right-0 md:bottom-0 md:mr-24 md:mb-16">
+      <h1 className="text-5xl text-white">Welcome To</h1>
+      <h2 className="text-3xl text-orange-500">YourCompanyName</h2>
     </div>
   </section>
 )
