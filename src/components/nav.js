ad89d639388a0bd2b801c320f4552823ccc5a55d
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 15:28:08 -0500

    Make nav fixed with hover styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 const Nav = () => (
-  <nav className="hidden md:block bg-black-transparent">
+  <nav className="hidden md:block bg-black-transparent fixed w-screen z-50">
     <ul className="flex px-12 justify-between lg:justify-start">
       <li className="text-gray-900 bg-blue-transparent px-4 py-4 uppercase">Home</li>
-      <li className="text-gray-200 px-4 py-4 uppercase">About</li>
-      <li className="text-gray-200 px-4 py-4 uppercase">Services</li>
-      <li className="text-gray-200 px-4 py-4 uppercase">Testimonials</li>
-      <li className="text-gray-200 px-4 py-4 uppercase">Portfolio</li>
-      <li className="text-gray-200 px-4 py-4 uppercase">Contacts</li>
+      <li className="text-gray-200 px-4 py-4 uppercase hover:text-gray-900 hover:bg-blue-transparent">About</li>
+      <li className="text-gray-200 px-4 py-4 uppercase hover:text-gray-900 hover:bg-blue-transparent">Services</li>
+      <li className="text-gray-200 px-4 py-4 uppercase hover:text-gray-900 hover:bg-blue-transparent">Testimonials</li>
+      <li className="text-gray-200 px-4 py-4 uppercase hover:text-gray-900 hover:bg-blue-transparent">Portfolio</li>
+      <li className="text-gray-200 px-4 py-4 uppercase hover:text-gray-900 hover:bg-blue-transparent">Contacts</li>
     </ul>
   </nav>
 )
