4cf0aae6f6441cd512bd626ad3ebb39357403b09
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-16 16:56:26 -0500

    Add styling to team-leaders section

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 const Card = ({ imgSrc, title, details }) => (
-  <div>
-    <img src={imgSrc} alt={title} />
-    <p>{title}</p>
-    <p>{details}</p>
+  <div className="text-center w-64">
+    {imgSrc && <img className="h-84" src={imgSrc} alt={title} />}
+    <div className="bg-gray-600 w-64 h-84"></div>
+    <p className="text-xl pt-6 pb-10">{title}</p>
+    <p className="text-sm pb-8">{details}</p>
   </div>
 )
 
--- src/components/team-leaders.js
+++ src/components/team-leaders.js
@@ -8,13 +8,17 @@ const TeamLeaders = () => {
   const [teamLeaders,] = useState(teamLeadersData)
 
   return (
-    <section>
-      <h1>Our Team Leaders</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Ratione omnis ipsam adipisci quidem magni enim incidunt fugiat 
-        dolorum quod nihil quas, cupiditate architecto beatae perspiciatis. 
-        Veniam modi exercitationem expedita consectetur.</p>
-      {teamLeaders.map(leader => <Card key={leader.name} imgSrc={leader.imgSrc} title={leader.name} details={leader.description} />)}
+    <section className="pt-32 pb-24 px-4 mx-auto">
+      <div className="text-center mx-auto max-w-2xl">
+        <h1 className="font-serif text-orange-500 text-4xl mb-16">Our Team Leaders</h1>
+        <p className="mb-20">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Ratione omnis ipsam adipisci quidem magni enim incidunt fugiat 
+          dolorum quod nihil quas, cupiditate architecto beatae perspiciatis. 
+          Veniam modi exercitationem expedita consectetur.</p>
+      </div>
+      <div className="mx-auto flex flex-wrap justify-around max-w-4xl">
+        {teamLeaders.map(leader => <Card key={leader.name} imgSrc={leader.imgSrc} title={leader.name} details={leader.description} />)}
+      </div>
     </section>
   )
 }
