2694648f1ba9ae586affc4c402fb1aac2cfa9b0e
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-17 07:52:33 -0500

    Card showing img if available

--- src/components/card.js
+++ src/components/card.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const Card = ({ imgSrc, title, details }) => (
   <div className="text-center w-64">
-    {imgSrc && <img className="h-84" src={imgSrc} alt={title} />}
-    <div className="bg-gray-600 w-64 h-84"></div>
+    {imgSrc ? 
+      <img className="h-84" src={imgSrc} alt={title} /> :
+      <div className="bg-gray-600 w-64 h-84"></div>
+    }
     <p className="text-xl pt-6 pb-10">{title}</p>
     <p className="text-sm pb-8">{details}</p>
   </div>
