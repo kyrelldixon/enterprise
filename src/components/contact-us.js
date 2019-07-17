b5eec655e1c79755a47c652579fe4eb9cc5cb411
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-17 07:51:53 -0500

    Update contact-us styles

--- src/components/contact-us.js
+++ src/components/contact-us.js
@@ -3,10 +3,12 @@ import React from "react"
 import ContactForm from "./contact-form"
 
 const ContactUs = () => (
-  <section>
-    <h2>Contact Us</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-      Ad animi, architecto vero aliquid maiores voluptatibus.</p>
+  <section className="bg-teal-900 text-white pt-32 pb-24 px-4">
+    <div className="text-center mx-auto max-w-2xl">
+      <h2 className="text-4xl font-serif mb-8 uppercase">Contact Us</h2>
+      <p className="mb-20 font-sm text-gray-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Ad animi, architecto vero aliquid maiores voluptatibus.</p>
+    </div>
     <ContactForm />
   </section>
 )
