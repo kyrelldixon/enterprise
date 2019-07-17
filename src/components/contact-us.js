0f14d02dad8685e04242b3963b11d95b387380ef
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-17 11:20:36 -0500

    Add Contact us styles

--- src/components/contact-us.js
+++ src/components/contact-us.js
@@ -3,8 +3,8 @@ import React from "react"
 import ContactForm from "./contact-form"
 
 const ContactUs = () => (
-  <section className="bg-teal-900 text-white pt-32 pb-24 px-4">
-    <div className="text-center mx-auto max-w-2xl">
+  <section className="bg-teal-900 text-white pt-32 pb-24 px-4 flex flex-col items-center">
+    <div className="text-center max-w-2xl">
       <h2 className="text-4xl font-serif mb-8 uppercase">Contact Us</h2>
       <p className="mb-20 font-sm text-gray-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Ad animi, architecto vero aliquid maiores voluptatibus.</p>
