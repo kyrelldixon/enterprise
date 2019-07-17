3a84502ff5cfd028a73dcc3792d99d41078373fe
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-17 11:21:05 -0500

    Add contact form styles

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -12,18 +12,25 @@ const ContactForm = () => {
   }
   
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Name" />
-      <select name="Category">
-        { categories.map(category => <option key={category} value={category.toLowerCase()}>{category}</option>) }
-      </select>
-      <input type="text" name="phone" placeholder="Phone" />
-      <select name="City">
-        { cities.map(city => <option key={city} value={city.toLowerCase()}>{city}</option>) }
-      </select>
-      <input type="email" name="email" placeholder="Email" />
-      <textarea name="message" />
-      <button type="submit">Contact us</button>
+    <form className="w-full max-w-2xl" onSubmit={handleSubmit}>
+      <div className="mb-4 flex flex-wrap justify-between">
+        <input className="w-full md:w-half-pad-1 appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" type="text" name="name" placeholder="Name" required />
+        <select className="w-full md:w-half-pad-1 appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" name="Category">
+          <option default>Select a category</option>
+          { categories.map(category => <option key={category} value={category.toLowerCase()}>{category}</option>) }
+        </select>
+      
+        <input className="w-full md:w-half-pad-1 appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" type="text" name="phone" placeholder="Phone" required />
+        <select className="w-full md:w-half-pad-1 appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" name="City">
+          <option default>Select a city</option>
+          { cities.map(city => <option key={city} value={city.toLowerCase()}>{city}</option>) }
+        </select>
+        <input className="w-full appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" type="email" name="email" placeholder="Email" required />
+        <textarea className="w-full h-40 appearance-none bg-transparent border-b border-gray-500 focus:outline-none focus:border-orange-500 mb-10" name="message" placeholder="Message" />
+        <div className="flex justify-end w-full">
+          <button className="bg-orange-600 hover:bg-orange-800 text-white font-bold py-2 px-6 uppercase text-sm focus:outline-none" type="submit">Contact us</button>
+        </div>
+      </div>
     </form>
   )
 }
